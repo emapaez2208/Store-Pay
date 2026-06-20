7e43ea41f2ebbbabd09a2d9797c3f191c4664138
emapaez2208 <emapaezspognardi@gmail.com>
2026-06-20 00:06:35 -0300

    start springboot

---
+++ src/main/java/com/emapaez/storepay/StorepayApplication.java
@@ -0,0 +1,13 @@
+package com.emapaez.storepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emapaez/storepay/StorepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emapaez.storepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
