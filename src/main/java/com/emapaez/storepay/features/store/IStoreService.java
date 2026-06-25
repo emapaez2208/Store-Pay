81b3e7a3854b5b25e33153a966640eb97537978d
emapaez2208 <emapaezspognardi@gmail.com>
2026-06-25 09:18:13 -0300

    Update IStoreService.java

--- src/main/java/com/emapaez/storepay/features/store/IStoreService.java
+++ src/main/java/com/emapaez/storepay/features/store/IStoreService.java
@@ -2,6 +2,7 @@ package com.emapaez.storepay.features.store;
 
 import com.emapaez.storepay.features.store.domain.dto.StoreRequest;
 import com.emapaez.storepay.features.store.domain.dto.StoreResponse;
+import com.emapaez.storepay.features.store.domain.dto.StoreUpdate;
 
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface IStoreService {
     StoreResponse create(StoreRequest request);
     public StoreResponse findByExternalId(UUID externalId);
     void delete(UUID externalId);
+    StoreResponse update(UUID externalId, StoreUpdate update);
+
 }
